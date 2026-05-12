44179b973b0ceace6afc3d35e9b7d5481a32e005
datadavev <605409+datadavev@users.noreply.github.com>
2026-05-12 12:17:33 -0400

    Updates to match store refactor

--- app/api_v1/hashstore.py
+++ app/api_v1/hashstore.py
@@ -28,6 +28,13 @@ except ImportError:
 HASHSTORE_PATH = pathlib.Path("/var/data/hashstore")
 HS_MODULE_NAME = "hashstore.filehashstore"
 HS_CLASS_NAME = "FileHashStore"
+WEB_DELIMITER = "|"
+
+
+def split_pidpath(pidpath: str, delimiter: str = "|") -> list[str]:
+    pathpid = pidpath.strip(delimiter)
+    parts = pathpid.split(delimiter)
+    return parts
 
 
 def get_media_type(path: str) -> str:
@@ -67,57 +74,24 @@ def get_hashstore(request: Request):
     return request.app.state.hashstore
 
 
-def split_pid(pid: str) -> tuple[str, list[str], str]:
-    pid = pid.strip()
-    parts = pid.split(" ", 1)
-    path = ""
-    path_parts = []
-    if len(parts) > 1:
-        path = parts[1].strip()
-        if path in ("", ".", hashstore.folderentry.PATH_DELIMITER):
-            path = ""
-        path_parts = path.split(hashstore.folderentry.PATH_DELIMITER)
-    pid = parts[0]
-    folder_pid = f"{pid} {path}" if path != "" else pid
-    return (pid, path_parts, folder_pid)
-
-
-def resolve_pidpath(hs: hashstore.HashStore, pid: str, path: list[str]):
-    pathpid = pid
-    if len(path) > 0:
-        pathpid = f"{pid} {hashstore.folderentry.PATH_DELIMITER.join(path)}"
-    # First try grabbing the full path.
-    try:
-        print(f"TRY 1 {pathpid}")
-        return hs.find_object(pathpid)
-    except hashstore.filehashstore_exceptions.PidRefsDoesNotExist:
-        pass
-    try:
-        print(f"TRY 2 {pathpid}")
-        return hs.find_object(path[0])
-    except hashstore.filehashstore_exceptions.PidRefsDoesNotExist:
-        pass
-    if len(path) == 0:
-        raise hashstore.filehashstore_exceptions.PidRefsDoesNotExist(
-            f"Unable to resolve {pathpid}"
-        )
-    print(f"TRY 3 {pathpid}")
-    return resolve_pidpath(hs, path[0], path[1:])
-
-
 @router.get("/info/{pid:path}")
 async def file_info(pid: str, hs=Depends(get_hashstore)):
     """Return information for request debugging."""
-    pid, path, folder_pid = split_pid(pid)
-    object_info_dict = hs.find_object(folder_pid)
-    media_type = get_media_type(object_info_dict.get("cid_object_path"))
-    return {
-        "pid": pid,
-        "folder_pid": folder_pid,
-        "path": path,
-        "media_type": media_type,
-        "object_info": object_info_dict,
-    }
+    path = hashstore.folderentry.split_pidpath(pid, delimiter=WEB_DELIMITER)
+    try:
+        object_info_dict = hs.resolve_pidpath(path)
+        media_type = get_media_type(object_info_dict.get("cid_object_path"))
+        return {
+            "pid": pid,
+            "path": path,
+            "media_type": media_type,
+            "object_info": object_info_dict,
+        }
+    except hashstore.filehashstore_exceptions.PidRefsDoesNotExist:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"message": "Not Found", "pid": pid},
+        )
 
 
 @router.get("/meta/{pid:path}")
@@ -129,24 +103,30 @@ async def system_metadata(pid: str, hs=Depends(get_hashstore)):
 
     Return of the actual file is delegated to Apache mod_xsendfile.
     """
-    pid, path, folder_pid = split_pid(pid)
-    object_info_dict = hs.find_object(folder_pid)
-    sys_meta_path = object_info_dict.get("sysmeta_path")
-    if sys_meta_path in [None, "Does not exist."]:
-        object_info_dict = hs.find_object(pid)
+    path = hashstore.folderentry.split_pidpath(pid, delimiter=WEB_DELIMITER)
+    try:
+        object_info_dict = hs.resolve_pidpath(path)
         sys_meta_path = object_info_dict.get("sysmeta_path")
         if sys_meta_path in [None, "Does not exist."]:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"System Metadata for {folder_pid} not found.",
-            )
-    media_type = "application/xml"
-    return Response(
-        headers={
-            "X-Sendfile": str(sys_meta_path),
-            "Content-Type": media_type,
-        }
-    )
+            object_info_dict = hs.find_object(pid)
+            sys_meta_path = object_info_dict.get("sysmeta_path")
+            if sys_meta_path in [None, "Does not exist."]:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"System Metadata for {pid} not found.",
+                )
+        media_type = "application/xml"
+        return Response(
+            headers={
+                "X-Sendfile": str(sys_meta_path),
+                "Content-Type": media_type,
+            }
+        )
+    except hashstore.filehashstore_exceptions.PidRefsDoesNotExist:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"message": "Not Found", "pid": pid},
+        )
 
 
 @router.head("/object/{pid:path}")
@@ -156,27 +136,30 @@ async def download_file(pid: str, hs=Depends(get_hashstore)):
     Return of the actual file is delegated to Apache mod_xsendfile.
     """
     # Instead of returning the file, we tell Apache where it is
-    pid, path, folder_pid = split_pid(pid)
-    object_info_dict = resolve_pidpath(hs, pid, path)
-    object_path = object_info_dict.get("cid_object_path")
-    if isinstance(object_path, str):
+    path = hashstore.folderentry.split_pidpath(pid, delimiter=WEB_DELIMITER)
+    try:
+        object_info_dict = hs.resolve_pidpath(path)
+        object_path = object_info_dict.get("cid_object_path")
         media_type = get_media_type(object_path)
-    else:
-        media_type = ""
 
-    if media_type == "application/json":
-        # peek at the file. If it's a proxy file, then
-        # redirect to the designated URL
-        with open(object_path, "r") as fsrc:
-            proxy_dict = json.load(fsrc)
-            redirect_url = proxy_dict.get("url")
-            if redirect_url is not None:
-                return RedirectResponse(
-                    url=redirect_url, status_code=status.HTTP_307_TEMPORARY_REDIRECT
-                )
-    return Response(
-        headers={
-            "X-Sendfile": str(object_path),
-            "Content-Type": media_type,
-        }
-    )
+        if media_type == "application/json":
+            # peek at the file. If it's a proxy file, then
+            # redirect to the designated URL
+            with open(object_path, "r") as fsrc:
+                proxy_dict = json.load(fsrc)
+                redirect_url = proxy_dict.get("url")
+                if redirect_url is not None:
+                    return RedirectResponse(
+                        url=redirect_url, status_code=status.HTTP_307_TEMPORARY_REDIRECT
+                    )
+        return Response(
+            headers={
+                "X-Sendfile": str(object_path),
+                "Content-Type": media_type,
+            }
+        )
+    except hashstore.filehashstore_exceptions.PidRefsDoesNotExist:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"message": "Not Found", "pid": pid},
+        )
